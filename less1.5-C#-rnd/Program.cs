9d0626cf7d141f1e8444e852499065a0e05f3f70
Algo <algor102@gmail.comm>
2023-02-01 02:10:24 +0500

    Убрал ошибку с Null в less1.2

---
+++ less1.5-C#-rnd/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Программа для вычисления суммы двух чисел");
+Console.WriteLine("Оба числа выбираются случайным методом от 1 до 9");
+double x = new Random().Next(1,10); // 1 2 3 4 ... 9
+double y = new Random().Next(1,10);
+double rezult;
+rezult = x + y;
+Console.Write("Первое число: ");
+Console.WriteLine(x);
+Console.Write("Второе число: ");
+Console.WriteLine(y);
+Console.Write("Результат суммирования двух чисел: ");
+Console.WriteLine(rezult);
