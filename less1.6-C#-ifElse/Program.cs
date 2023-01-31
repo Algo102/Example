c2a8eccbfb2b1f68202794f02df71073a05ec83c
Algo <algor102@gmail.comm>
2023-02-01 04:51:37 +0500

    less1-progr6-enter user

---
+++ less1.6-C#-ifElse/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Привет, пожалуйста представьтесь");
+string? username = Console.ReadLine();
+if (username.ToLower() == "саша")
+  Console.WriteLine("Добро пожаловать Сашуля"); //Фигурные скобки можно не ставить, если одна строка
+else
+{
+  Console.WriteLine("Привет," + " " + username + " че надо?");
+}
