6b224d39b6b9101458b81d0b529c8cf4d06f0c1b
Algo <algor102@gmail.comm>
2023-02-01 02:24:53 +0500

    Доработал программу 5 с суммированием случайных чисел

--- less1.5-C#-rnd/Program.cs
+++ less1.5-C#-rnd/Program.cs
@@ -4,9 +4,6 @@ double x = new Random().Next(1,10); // 1 2 3 4 ... 9
 double y = new Random().Next(1,10);
 double rezult;
 rezult = x + y;
-Console.Write("Первое число: ");
-Console.WriteLine(x);
-Console.Write("Второе число: ");
-Console.WriteLine(y);
-Console.Write("Результат суммирования двух чисел: ");
-Console.WriteLine(rezult);
+Console.Write("Первое число: " + x);
+Console.Write("\nВторое число: " + y); // \n перевод корретки на новую строку
+Console.Write("\nРезультат суммирования двух чисел: " + rezult);
