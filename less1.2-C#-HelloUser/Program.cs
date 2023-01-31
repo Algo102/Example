7f887c379e0f78e349510f1706e26e3d1f263837
Algo <algor102@gmail.comm>
2023-02-01 02:11:41 +0500

    Убрал ошибку с Null в less1.2 и добавил 5 программу с рандомом

--- less1.2-C#-HelloUser/Program.cs
+++ less1.2-C#-HelloUser/Program.cs
@@ -1,5 +1,5 @@
 ﻿
 Console.WriteLine("Comes tu llamo");
 string? username = Console.ReadLine(); //Достаточно сделать переменную наллейбел добавив ? к типу объявляемой переменной
-Console.Write("Hola  ");
+Console.Write("Hola ");
 Console.WriteLine(username);
