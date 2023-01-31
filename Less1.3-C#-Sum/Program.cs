f46004932b9c6d28dc6317ef978bc3b90b904bd3
Algo <algor102@gmail.comm>
2023-02-01 00:02:54 +0500

    Lesson 1 - progr 3 - Sum

---
+++ Less1.3-C#-Sum/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Программа для вычисления суммы двух целых чисел");
+Console.WriteLine("Введите первое число: ");
+int x = int.Parse(Console.ReadLine ());
+Console.WriteLine("Введите второе число: ");
+int y = int.Parse(Console.ReadLine ());
+int sum;
+sum = x + y;
+Console.Write("Сумма двух чисел равна: ");
+Console.WriteLine(sum);
