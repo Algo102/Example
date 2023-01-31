a6eb049335669188ba00ab2a824c81e311482daa
Algo <algor102@gmail.comm>
2023-02-01 00:26:34 +0500

    Less1-prog4-delenie

---
+++ less1.4-C#-Delenie/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Программа для вычисления частного двух чисел");
+Console.WriteLine("Введите первое число: ");
+double x = double.Parse(Console.ReadLine ());
+Console.WriteLine("Введите второе число: ");
+double y = double.Parse(Console.ReadLine ());
+double rezult;
+rezult = x / y;
+Console.Write("Результат деления двух чисел: ");
+Console.WriteLine(rezult);
