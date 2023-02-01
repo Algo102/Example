7ee86e9a553423862da120bb4504cda7bd5b5554
Algo <algor102@gmail.comm>
2023-02-01 07:40:52 +0500

    Less1 - prorr7 - giri

--- less1.6-C#-ifElse/Program.cs
+++ less1.6-C#-ifElse/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Привет, пожалуйста представьтесь");
 string? username = Console.ReadLine();
-if (username.ToLower() == "саша")
+if (username.ToLower() == "саша") //ToLower, понижает в нижний регистр
   Console.WriteLine("Добро пожаловать Сашуля"); //Фигурные скобки можно не ставить, если одна строка
 else
 {
---
+++ less1.7-C#-Giri/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Программа ищет самую тяжелую гирю из 5");
+int a = 9;
+int b = 8;
+int c = 3;
+int d = 2;
+int e = 4;
+int maxim = a;
+if (b > maxim)
+  maxim = b;
+if (c > maxim)
+  maxim = c;
+if (d>maxim)
+  maxim = d;
+if (e>maxim)
+  maxim = e;
+Console.WriteLine ("Самая тяжелая гиря имеет вес: " + maxim);
