d6a27944c8e126447a80740ca19584beb43fa86b
Algo <algor102@gmail.comm>
2023-02-03 07:04:16 +0500

    Семминар1 - Задача2 - Больше меньше

---
+++ Semminar1.2-bolsheMenshe/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Программа определяет какое число из двух больше");
+
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()); 
+
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()); 
+
+if (number1>number2) // int max = number1 > number2 ? number1 : number2
+{
+  Console.WriteLine("Первое число " + number1 + " > " + "второго числа " + number2);
+}
+else 
+{
+  if (number2>number1)
+  {
+    Console.WriteLine("Второе число " + number2 + " > " + "первого числа " + number1);
+  }
+  else
+  {
+    Console.WriteLine("Ваши числа равные");
+  }
+}
+
