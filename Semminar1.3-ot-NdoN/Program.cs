4b445e79dac32fc144642c1184669e06e9ed1af1
Algo <algor102@gmail.comm>
2023-02-03 08:04:27 +0500

    Семминар1 - Задача3 - Вывод чисел с шагом 1 от -N до N

---
+++ Semminar1.3-ot-NdoN/Program.cs
@@ -0,0 +1,14 @@
+﻿//Console.Clear;
+Console.WriteLine("Программа с шагом 1 идет до модуля этого числа");
+
+Console.WriteLine("Введите отрицательное целое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine()); 
+
+int step = number1*(-1)+1;
+
+
+while (number1 != step)
+{
+  Console.Write(number1 + "  ");
+  number1++;
+}
