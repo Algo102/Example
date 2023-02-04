b872405dd55b4293e2843f4bf5bc1261c6afd29f
Algo <algor102@gmail.comm>
2023-02-05 00:52:44 +0500

    Лекц2-прогр3-поиск элемента по значению

---
+++ 2.3Less-findElem/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Программа ищет нужное число в массиве и указывает его элемент");
+int []array = {15, 24, 28, 34, 43, 25, 36, 57, 28};
+
+int n = array.Length;
+int find = 43;
+int index = 0;
+
+while (index <n)
+{
+  if (array[index] == find)
+  {
+    Console.WriteLine("Ваш элемент " + index);
+    break; //благодаря брейку, не ищет после нахождения первого совпадения
+  }
+    
+  index++;
+}
