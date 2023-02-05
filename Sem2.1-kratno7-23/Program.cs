43c509a786cdef85e5ef6336864f871c275f7083
Algo <algor102@gmail.comm>
2023-02-06 04:22:26 +0500

    Семминар2-прогр1-Вееденое число кратно 7 и 23?

---
+++ Sem2.1-kratno7-23/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Программа проверяет делится ли введеное число на 7 и 23");
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int del7 = number % 7;
+int del32 = number % 23;
+
+if (del7 == 0 & del32 == 0)
+{
+  Console.WriteLine("Ваше число делится на 7 и 23");
+}
+else
+{
+  Console.WriteLine("Ваше число не делится на 7 и 23");
+}
