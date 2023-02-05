6ef1e98b869b59d1168fc0bf5b4296d772dc4eec
Algo <algor102@gmail.comm>
2023-02-06 04:48:24 +0500

    Сем2-прогр2 - определяем в какой четверти точка

---
+++ Sem2.2-koordinats/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Программа определяет в какой части координат лежит точка");
+
+Console.WriteLine("Введите X");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите Y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x>0 & y>0) Console.WriteLine("Ваша точка в I четверти");
+else if (x<0 & y>0) Console.WriteLine("Ваша точка во II четверти");
+else if (x<0 & y<0) Console.WriteLine("Ваша точка в III четверти");
+else Console.WriteLine("Ваша точка в IV четверти");
