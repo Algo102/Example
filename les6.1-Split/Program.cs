f470fcdfd58600fe859c415df2bd603aa01e1d8e
Algo <algor102@gmail.comm>
2023-02-22 04:01:47 +0500

    Пример работы со СПЛИТ и массивом строк

---
+++ les6.1-Split/Program.cs
@@ -0,0 +1,19 @@
+﻿// Работа со сплитом и массивом строк
+
+using System.Linq;
+
+string text = "(1,2) (2,3) (4,5) (6,7)"
+        .Replace("(", "") // убрали скобки
+        .Replace(")", "");
+
+Console.WriteLine(text);
+Console.WriteLine();
+
+//string[] data = text.Split(" ")
+var data = text.Split(" ") //по пробелу перенесли строками в массив
+        .ToArray();
+
+for(int i = 0; i < data.Length; i++)
+{
+  Console.WriteLine(data[i]);
+}
