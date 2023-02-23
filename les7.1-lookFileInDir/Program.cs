2e66067a815ba90d4190b4e01b01626bb1716dde
Algo <algor102@gmail.comm>
2023-02-23 06:38:00 +0500

    Сканирование дерикторий

---
+++ les7.1-lookFileInDir/Program.cs
@@ -0,0 +1,9 @@
+﻿string path = "C:/Users/Algor/Desktop/ГикБрайнс/1 четверть/Знакомство с языками программирования/Examples/les7.1-lookFileInDir";
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CretionTime);
+// FilInfo[] fi = di.GetFiles();
+
+// for(int i = 0; i < fi.Length; i++)
+// {
+//   System.Console.WriteLine(fi[i].Name);
+// }
